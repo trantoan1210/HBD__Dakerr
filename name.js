93d674ad2f98ed9a312b4bb87d67e3585e94707b
Toàn Trần <neo290992@gmail.com>
2021-10-12 23:05:09 +0700

    pha

--- name.js
+++ name.js
@@ -1,8 +1,19 @@
+let x=0
 function name__123(){
     name__main=document.getElementById('login__name').value;
    
     if(name__main==''){
-        alert('Nhập chữ hộ bố mài')
+        switch (x){
+            case 4:{ alert('Nhập đê ko biết chữ sao ko nhập')
+            break;}
+            case 8: {alert('cố chấp thế nhở, nhập chữ nào')
+            break;
+        }
+            default :{alert('Nhập chữ hộ bố mài')
+    
+        }
+        }
+        x++
     }else{ 
         document.getElementById('box__login').style.display='none';
     document.getElementById('name').innerHTML= name__main
