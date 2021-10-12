83c39d150f11654c3161cf5afebd72bec754b3ac
Toàn Trần <neo290992@gmail.com>
2021-10-12 20:51:03 +0700

    tạo

---
+++ name.js
@@ -0,0 +1,5 @@
+function name__123(){
+    name__main=document.getElementById('login__name').value;
+    document.getElementById('box__login').style.display='none';
+    document.getElementById('name').innerHTML= name__main
+}
