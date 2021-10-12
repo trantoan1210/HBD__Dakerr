ac48dcde9fe1e19c9f38babaf2d25db700c83dd2
Toàn Trần <neo290992@gmail.com>
2021-10-12 22:34:56 +0700

    capnhat

--- name.js
+++ name.js
@@ -1,5 +1,8 @@
 function name__123(){
     name__main=document.getElementById('login__name').value;
     document.getElementById('box__login').style.display='none';
+    if(name__main==''){
+        alert('Nhập chữ hộ bố mài')
+    }else{
     document.getElementById('name').innerHTML= name__main
-}
+}}
